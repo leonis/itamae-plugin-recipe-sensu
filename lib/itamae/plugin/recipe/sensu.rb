9119fd49dfb4df18184be018228ecb692d1d6abe
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-05-11 13:58:36 +0900

    initial import

---
+++ lib/itamae/plugin/recipe/sensu.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/sensu/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Sensu
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/sensu/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Sensu
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
