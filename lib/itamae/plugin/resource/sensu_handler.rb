5ef2fbccad7a652fb13595cac9b083a123cead74
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-05-18 14:21:22 +0900

    add sensu_handler resource

---
+++ lib/itamae/plugin/resource/sensu_handler.rb
@@ -0,0 +1,45 @@
+require 'itamae'
+require 'json'
+require 'pathname'
+
+module Itamae
+  module Resource
+    class SensuHandler < File
+      define_attribute :action, default: :create
+      define_attribute :path, type: String, default_name: false
+      define_attribute :handler_name, type: String, default_name: true
+      define_attribute :type, type: String
+      define_attribute :command, type: String
+      define_attribute :severities, type: Array, default: []
+      define_attribute :additional, type: Hash, default: {}
+
+      def pre_action
+        attributes.content = handler_json
+
+        # FIXME: fetch handler path from node and set new path if path is nil.
+        config_name = attributes.handler_name + '.json'
+        attributes.path ||= Pathname.new(node.sensu.directory).join('conf.d/handlers', config_name)
+        attributes.mode = '0644'
+        attributes.owner ||= 'sensu'
+        attributes.group ||= 'sensu'
+
+        super
+      end
+
+      private
+
+      def handler_json
+        hash = {
+          handlers: {
+            attributes.handler_name => {
+              type: attributes.type,
+              command: attributes.command
+            }.merge(attributes.additional)
+          }
+        }
+
+        JSON.pretty_generate(hash)
+      end
+    end
+  end
+end
